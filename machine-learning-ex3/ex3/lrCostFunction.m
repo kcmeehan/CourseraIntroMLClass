5d99e829f7094186fe372ab59795319176cbb87e
Kathryn Meehan <katya.nuclear@gmail.com>
2018-09-10 18:36:18 -0700

    first commit of ex3 directory AND completed lrCostFunction.m file

---
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -0,0 +1,35 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+h = sigmoid(X*theta);
+[r, c] = size(theta);
+theta_temp = theta; 
+theta_temp(1) = 0; 
+J = -(y'*log(h) + (1-y)'*log(1-h))./m + lambda*(theta_temp'*theta_temp)/(2*m);
+
+grad = (X'*(h-y) + lambda*theta_temp)./m
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+% =============================================================
+
+grad = grad(:);
+
+end
