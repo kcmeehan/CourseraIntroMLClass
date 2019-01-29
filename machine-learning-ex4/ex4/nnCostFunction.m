89d8455bfbde7ef2507abebebe4698d0879e68ce
Kathryn Meehan <katya.nuclear@gmail.com>
2019-01-29 08:43:47 -0800

    first attempt at backpropagation

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -39,25 +39,29 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 X  = [ones(m, 1) X]; %% size: 5000 x 401
-a2 = sigmoid(X * Theta1'); %% size: 5000 x 25
+Y = zeros(m, 10); %% size: 5000 x 10
+
+%% Forward propagation
+z2 = X * Theta1';
+a2 = sigmoid(z2); %% size: 5000 x 25
 a2 = [ones(m, 1) a2]; %% size: 5000 x 26
 a3 = sigmoid(a2 * Theta2'); %% size: 5000 x 10
 h = a3; %% 5000 x 10
 
 for i=1:m
   index = y(i);	
-  yvec  = zeros(1, num_labels); %% size: 1 x 10
-	yvec(index) = 1;
+	Y(i, index) = 1;
+  yvec  = Y(i, :); %% size: 1 x 10
 	hvec  = h(i, :); %%1 X 10
 	J = J -(yvec*log(hvec') + (1-yvec)*log((1-hvec)'))/m;
 end
 
 Theta1_temp = Theta1(:, 2:input_layer_size+1);
 Theta2_temp = Theta2(:, 2:hidden_layer_size+1);
-Theta1_sum = Theta1_temp.^2;
-Theta2_sum = Theta2_temp.^2;
-Theta1_sum = sum(Theta1_sum(:));
-Theta2_sum = sum(Theta2_sum(:));
+Theta1_sq = Theta1_temp.^2;
+Theta2_sq = Theta2_temp.^2;
+Theta1_sum = sum(Theta1_sq(:));
+Theta2_sum = sum(Theta2_sq(:));
 
 J = J + lambda/(2*m) * (Theta1_sum + Theta2_sum);
 
@@ -75,6 +79,30 @@ J = J + lambda/(2*m) * (Theta1_sum + Theta2_sum);
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
+
+%% Determining dJ/dTheta(2):
+delta_3 = zeros(m, num_labels);
+Delta_2 = zeros(num_labels, hidden_layer_size + 1);
+
+delta_3 = a3 - Y; 
+Delta_2 = delta_3' * a2;
+Theta2_grad = Delta_2./m;
+
+%% Determining dJ/dTheta(1):
+delta_2 = zeros(m, hidden_layer_size + 1);
+Delta_1 = zeros(hidden_layer_size, input_layer_size + 1);
+
+delta_2 = delta_3 * Theta2; %%size: 5000 x 26
+z2 = [ones(m, 1) z2];
+delta_2 = delta_2 .* sigmoidGradient(z2);
+Delta_1 = delta_2' * X;
+Theta1_grad = Delta_1./m;
+
+
+
+%for i=1:m
+
+
 %
 % Part 3: Implement regularization with the cost function and gradients.
 %
